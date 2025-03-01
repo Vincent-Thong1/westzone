113bbf16abe56ec3290a9ca5ab44a079b1ae984f
Vincent-Thong1 <wengsoon.thong@dyson.com>
2025-03-01 11:26:42 +0800

    Add files via upload

--- assets/js/headerFooterManager.js
+++ assets/js/headerFooterManager.js
@@ -8,7 +8,7 @@ class CommonHeader extends HTMLElement {
             <i class="fa fa-caret-down"></i>
           </button>
           <div class="dropdown-content" id="myDropdown">
-            <a href="https://xiangqi.xyz/赛事/2025/棋友杯2025.html">棋友杯2025</a>
+            <a href="https://xiangqi.xyz/赛事/2025/qiyoubei2025.html">棋友杯2025</a>
           </div>
         </div>
         <a href="https://xiangqi.xyz/tournament.html">近期赛事</a>
@@ -39,15 +39,15 @@ customElements.define('common-footer', CommonFooter)
 function setActivePage() {
   const currentPage = window.location.pathname.split('/').pop();
   const navLinks = document.querySelectorAll('common-header div a');
-  // console.log(currentPage)  // for debugging purpose
+  console.log(currentPage)  // for debugging purpose
 
   navLinks.forEach(link => {
     const linkPage = link.getAttribute('href');
     _linkPage = linkPage.split('/').pop()
-    // console.log(_linkPage + " compared with " + currentPage)  // for debugging purpose
+    console.log(_linkPage + " compared with " + currentPage)  // for debugging purpose
     if (_linkPage === currentPage) {
       link.classList.add('active');
-      // console.log(_linkPage + " matches " + currentPage)  // for debugging purpose
+      console.log(_linkPage + " matches " + currentPage)  // for debugging purpose
     }
   });
 }
